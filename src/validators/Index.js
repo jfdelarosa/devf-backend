59b186c89016fcb93adb7896ae05ec79ea6a75af
Juan Fernando <jfdelarosa@outlook.com>
2020-12-09 22:41:18 -0600

    Test

--- src/validators/Index.js
+++ src/validators/Index.js
@@ -1,6 +1,7 @@
 const AuthValidator = require("./AuthValidator");
 const CompanyValidator = require("./CompanyValidator");
 
+// TODO: Delete this comment
 module.exports = {
   AuthValidator,
   CompanyValidator,
