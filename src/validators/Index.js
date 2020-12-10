02645855943ddf268e813ba481b3d7680c4f205f
Juan Fernando <jfdelarosa@outlook.com>
2020-12-09 22:42:26 -0600

    Se crea un nuevo index.js

--- src/validators/Index.js
+++ src/validators/Index.js
@@ -1,7 +1,6 @@
 const AuthValidator = require("./AuthValidator");
 const CompanyValidator = require("./CompanyValidator");
 
-// TODO: Delete this comment
 module.exports = {
   AuthValidator,
   CompanyValidator,
